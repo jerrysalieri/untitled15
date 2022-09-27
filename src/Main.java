288a69e791518ff0aecd5381e53becdf19b983a5
jerrysalieri <112954102+jerrysalieri@users.noreply.github.com>
2022-09-27 11:33:00 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[]args){
+        int size =10;
+       int[]firstArray=new int[10];
+       int[]secondArray=new int[size];
+        double[]thirdArray=new double[10];
+        int[]fourthArray={1,2,3,4,5};
+        int length = firstArray.length;
+        System.out.println(length);
+        firstArray[4]=45;
+        System.out.println("5th element value: " + firstArray[4]);
+       // System.out.println(firstArray[999]);
+        for(int i=0; i<secondArray.length; i++){
+
+            System.out.println("Input your number");
+            Scanner sc=new Scanner(System.in);
+            String inputData=sc.nextLine();
+            int num=Integer.parseInt(inputData);
+            secondArray[i]=num;
+        }
+        for(int i=0; i<secondArray.length;i++){
+            System.out.print(secondArray[i]);
+        }
+
+    }
 }
